ef3227124337f1d004df38d0af7765c4f78fc3a4
juliette legrand <60143204+jlegrand35@users.noreply.github.com>
2021-12-15 16:27:32 +0100

    Add files via upload

--- ExtremesBinaryClassifier/R/functions.R
+++ ExtremesBinaryClassifier/R/functions.R
@@ -1,18 +1,5 @@
 rm(list = ls()) #Delete objects from the memory
 
-#' Unit-Frechet transformation
-#'
-#' Transforms data to unit-Frechet scale using rank transformation
-#' @param X a numeric vector
-#' @references Legrand et al.
-#' @examples
-#' FrechetMargin()
-FrechetMargin = function(X){
-  probs <- rank(X, na.last = "keep") /
-    (sum(!is.na(X)) + 1)
-  Xf <- - 1 / log(probs)
-  return(Xf)
-}
 
 #' A Risk estimation function
 #'
@@ -21,7 +8,7 @@ FrechetMargin = function(X){
 #' @param Y.eps vector of the true binary outcomes in the extreme region
 #' @param g vector of the predicted binary outcomes from a given classifier
 #' @param g.eps vector of the predicted binary outcomes in the extreme region from the same classifier
-#' @param epsilon single numeric between 0 and 1 giving the amount of data we want to remove
+#' @param epsilon logical value indicating whether the classic risk function should be used or the extended version
 #' @references Legrand et al.
 #' @details to add
 #' @seealso \link{LinearClassifier}
@@ -47,29 +34,52 @@ FrechetMargin = function(X){
 #' Htest <- H[-ii]
 #'
 #' ## Linear classifier
-#' ## Train the linear classifier
-#' init <- lm(H ~ X1 + X2, data = data.frame(H = H, X1 = X1, X2 = X2))$coefficients[2:3]
-#' linclass <- LinearClassifier(X = Xtrain, thresh = eps_u, H = Htrain, initials = init)
-#' ## Compute the predicted binary outcome on the test set from all the data and only with the extreme region data
-#' glin <- 2*(as.vector(linclass$theta %*% t(Xtest)) > u) - 1
-#' glineps <- 2*(as.vector(linclass$theta %*% t(Xtest)) > eps_u) - 1
+#' #~~~~~~~~~~~~~~~~~~~
+#' ## Train the linear classifier with mass
+#' init0 <- lm(H ~ X1 + X2, data = data.frame(H = H, X1 = X1, X2 = X2))$coefficients[2:3]
+#' linclass <- LinearClassifier(X = Xtrain, thresh = u, H = Htrain, initials = init0, epsilon=0)$theta
+#' ## Compute the predicted binary outcome on the test set from all the data
+#' glin <- 2*(as.vector(linclass %*% t(Xtest)) > u) -1
+#' #~~~~~~~~~~~~~~~~~~~
+#' ## Train the linear classifier without mass
+#' glin0 <- init0 %*% t(cbind(X1,X2))
+#' init <- lm(H ~ X1 + X2, data=data.frame(H = H[H>eps_u & glin0>eps_u], X1 = X1[H>eps_u & glin0>eps_u],
+#'           X2 = X2[H>eps_u & glin0>eps_u]))$coefficients[2:3]
+#' linclass.eps <- LinearClassifier(X = Xtrain[,c(1,2)], thresh = u, H = Htrain, initials = init,
+#'                   epsilon = eps)$theta
+#' ## Compute the predicted binary outcome on the test set from the extreme region data
+#' glineps <- 2*(as.vector(linclass.eps %*% t(Xtest)) > eps_u) - 1
+#' #~~~~~~~~~~~~~~~~~~~
 #' ## Compute the true binary outcome on the test set from all the data and only with the extreme region data
 #' Ytesteps <- 2*(Htest > eps_u) - 1
 #' Ytest <- 2*(Htest > u) - 1
-#' EmpiricalRisk(Y = Ytest, Y.eps = Ytesteps, g = glin, g.eps = glineps, epsilon = eps)
+#' #~~~~~~~~~~~~~~~~~~~
+#' ## Compute the associated risk
+#' EmpiricalRisk(Y = Ytest, Y.eps = Ytesteps, g = glin, g.eps = glineps, epsilon = TRUE)
 #'
 #' ## Comparison with regression tree
+#' #~~~~~~~~~~~~~~~~~~~
+#' ## Train the tree classifier with mass
+#' Ytrain <- 2*(Htrain > u) - 1
+#' treeclass <- rpart(y~., data=data.frame(x = Xtrain, y = as.factor(Ytrain)), method = "class")
+#' ## Compute the predicted binary outcome on the test set from all the data
+#' gtree <- as.numeric(as.character(predict(treeclass, newdata = data.frame(x = Xtest, y = Ytest),
+#'            type="class")))
+#' #~~~~~~~~~~~~~~~~~~~
+#' ## Train the tree classifier without mass
 #' Ytraineps <- 2*(Htrain > eps_u) - 1
-#' treeclass <- rpart(y~., data=data.frame(x=Xtrain, y = as.factor(Ytraineps)), method = "class")
-#' gtree <- as.numeric(as.character(predict(treeclass, newdata = data.frame(x = Xtest, y = Ytest), type="class")))
-#' gtreeeps <- as.numeric(as.character(predict(treeclass,
-#' newdata = data.frame(x = Xtest, y = Ytesteps), type = 'class')))
-#' EmpiricalRisk(Y = Ytest, Y.eps = Ytesteps, g = gtree, g.eps = gtreeeps, epsilon = eps)
-EmpiricalRisk = function(Y, Y.eps = NULL, g, g.eps = NULL, epsilon = 0){
+#' treeclasseps <- rpart(y~., data=data.frame(x = Xtrain, y = as.factor(Ytraineps)), method = "class")
+#' ## Compute the predicted binary outcome on the test set from the extreme region data
+#' gtreeeps <- as.numeric(as.character(predict(treeclasseps, newdata = data.frame(x = Xtest, y = Ytesteps),
+#'               type = 'class')))
+#' #~~~~~~~~~~~~~~~~~~~
+#' ## Compute the associated risk
+#' EmpiricalRisk(Y = Ytest, Y.eps = Ytesteps, g = gtree, g.eps = gtreeeps, epsilon = TRUE)
+EmpiricalRisk = function(Y, Y.eps = NULL, g, g.eps = NULL, epsilon = TRUE){
   stopifnot(length(Y) == length(g))
   stopifnot(length(Y.eps) == length(g.eps))
-  stopifnot(epsilon >= 0)
-  if (epsilon > 0){
+  stopifnot(is.logical(epsilon))
+  if (epsilon == TRUE){
     Risk <- sum((g != Y) & (Y.eps == 1) & (g.eps == 1))/
       sum((g == 1 | Y == 1) &
             (Y.eps == 1) & (g.eps == 1))
@@ -88,6 +98,7 @@ EmpiricalRisk = function(Y, Y.eps = NULL, g, g.eps = NULL, epsilon = 0){
 #' @param thresh single numeric giving the threshold over which an extreme event is defined
 #' @param H numeric vector corresponding to the latent variable that we wish to predict
 #' @param initials initial values for the parameters of the linear classifier to be optimized over
+#' @param epsilon single numeric giving the amount of data to remove
 #' @references Legrand et al.
 #' @return theta value of the linear classifier
 #' \item{theta}{the optimal parameters for the linear classifier}
@@ -102,14 +113,16 @@ EmpiricalRisk = function(Y, Y.eps = NULL, g, g.eps = NULL, epsilon = 0){
 #' H <- X1 + P
 #' u <- quantile(H,probs=0.97)
 #' init <- lm(H ~ X1 + X2, data=data.frame(H = H, X1 = X1, X2 = X2))$coefficients[2:3]
-#' LinearClassifier(X = cbind(X1, X2), thresh = u, H = H, initials = init)
-LinearClassifier = function(X, thresh, H, initials){
+#' LinearClassifier(X = cbind(X1, X2), thresh = u, H = H, initials = init, epsilon = 0)
+LinearClassifier = function(X, thresh, H, initials, epsilon){
   stopifnot(length(initials)==ncol(as.matrix(X)))
   stopifnot(length(H)==nrow(as.matrix(X)))
+  stopifnot(epsilon >= 0)
+  stopifnot(thresh >= 0)
   nvar <- ncol(as.matrix(X))
   init <- log(abs(initials))
   res <- optim(par = init, fn = EmpRiskLin, X = X,
-               thresh = thresh, H = H,
+               thresh = thresh, H = H, epsilon = epsilon,
                control = list(parscale = rep(1e-2,nvar)),
                method = 'SANN')
   return(list("theta" = exp(res$par), "Risk" = res$value))
@@ -125,17 +138,27 @@ LinearClassifier = function(X, thresh, H, initials){
 #' @param thresh single numeric giving the threshold over which an extreme event is defined
 #' @param H numeric vector corresponding to the latent variable that we wish to predict
 #' @param theta parameters for the linear classifier
+#' @param epsilon single numeric giving the amount of data to remove
 #' @return The empirical risk of the linear classifier with theta coefficients is computed
 #' @noRd
-EmpRiskLin = function(theta, X, thresh, H){
+EmpRiskLin = function(theta, X, thresh, H, epsilon){
   if (length(theta)!=ncol(as.matrix(X)) |
       length(H)!=nrow(as.matrix(X))){
     stop("H, X and theta must have same length.")}
+  stopifnot(epsilon >= 0)
   exp.theta <- exp(theta)
   g <- as.vector(t(exp.theta) %*% t(X))
   g <- 2*(g > thresh) - 1
   Y <- 2*(H > thresh) - 1
-  Risk <-  EmpiricalRisk(Y = Y, g = g)
+  if (epsilon > 0){
+    g.eps <- 2*(g > thresh*epsilon) - 1
+    Y.eps <- 2*(H > thresh*epsilon) - 1
+    Risk <-  EmpiricalRisk(Y = Y, g = g, g.eps = g.eps, Y.eps = Y.eps,
+                           epsilon = TRUE)
+  }
+  else{
+    Risk <-  EmpiricalRisk(Y = Y, g = g, epsilon = FALSE)
+  }
   return(Risk)
 }
 
